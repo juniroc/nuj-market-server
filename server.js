e5c642665153a2950f1f974ad37b05b4fc3fda2e
juniroc <lmj35021@gmail.com>
2023-03-03 23:41:55 +0900

    fix: add upload function

--- server.js
+++ server.js
@@ -1,19 +1,106 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const models = require("./models");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
 const port = 8080;
 
 app.use(express.json());
 app.use(cors());
 
+app.use("/uploads", express.static("uploads")); // 업로드시 파일이 뜨게하려면
+
 app.get("/products", (req, res) => {
-  res.send("업로드 된 상품입니다.");
+  models.Product.findAll({
+    // limit: 2,
+    order: [["createdAt", "DESC"]],
+    attributes: ["id", "name", "price", "createdAt", "seller", "imageUrl"],
+  })
+    .then((result) => {
+      console.log("PRODUCTS: ", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("에러 발생");
+    });
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageUrl: file.path,
+  });
 });
 
 app.post("/products", (req, res) => {
-  res.send("상품이 등록되었습니다.");
+  const body = req.body;
+  const { name, description, price, seller, imageUrl } = body;
+  if (!name || !description || !price || !seller || !imageUrl) {
+    res.status(400).send("모든 필드를 입력해주세요");
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+    imageUrl,
+  })
+    .then((result) => {
+      console.log("상품 생성 결과 : ", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품 업로드에 문제가 발생했습니다");
+    });
+});
+
+app.get("/products/:id", (req, res) => {
+  const params = req.params;
+  const { id } = params;
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      console.log("PRODUCT : ", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품 조회에 에러가 발생했습니다.");
+    });
 });
 
 app.listen(port, () => {
   console.log("Nuj의 쇼핑몰 서버가 돌아가고 있습니다.");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("DB 연결 성공");
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("DB 연결 에러 ㅠ");
+      process.exit();
+    });
 });
